17e26261982dc575c6e0d9377f8c380109083b21
Солдатов Николай Леонидович <soldatov@eos.ru>
2020-11-20 03:41:04 +0300

    Оптимизировал генератор форм

--- example/src/AjaxClientFormPage.tsx
+++ example/src/AjaxClientFormPage.tsx
@@ -2,50 +2,39 @@ import React, { FunctionComponent, useRef } from 'react'
 
 
 import "eos-webui-controls/dist/main.css";
-import { AjaxClientForm, FormMode } from "eos-webui-formgen";
-import { Button } from '@eos/rc-controls';
+import { AjaxClientForm, FormMode, parseFormMode } from "eos-webui-formgen";
+import { Helper } from './Helper';
+import { useRouteMatch } from 'react-router-dom';
 
+interface IPageParams {
+    id?: string;
+    mode?: string;
+}
 
 const AjaxClientFormPage: FunctionComponent = () => {
     const E_DOCUMENT_LABEL = "Для электронных документов";
+    const { params } = useRouteMatch<IPageParams>();
+    const mode: FormMode = parseFormMode(params.mode);
+    const id: number | undefined = params?.id ? parseFloat(params?.id) : undefined;
+
     const dataService: AjaxClientForm.IDataService = {
         async getContextAsync(mode: FormMode) {
             const dispContext = {
-                "Fields": [{ "disabled": true, "label": "inventory:fieldNames.parentName", "name": "parentName", "required": false, "requiredMessage": null, "type": "FieldText", "value": null, "additionalText": null, "allowClear": false, "maxLength": null }, { "disabled": false, "label": "inventory:fieldNames.ind", "name": "ind", "required": true, "requiredMessage": "inventory:formErrors.ind", "type": "FieldText", "value": null, "additionalText": null, "allowClear": true, "maxLength": 24 }, { "disabled": false, "label": "inventory:fieldNames.volumeNum", "name": "volumeNum", "required": false, "requiredMessage": null, "type": "FieldInteger", "value": null, "max": 32767, "min": 0, "showCounter": false }, { "disabled": false, "label": "inventory:fieldNames.keepCategory", "name": "keepCategory", "required": true, "requiredMessage": "inventory:formErrors.keepCategory", "type": "FieldSelect", "value": null, "values": [{ "key": "1", "value": "fields:keepCategory.1" }, { "key": "2", "value": "fields:keepCategory.2" }, { "key": "3", "value": "fields:keepCategory.3" }, { "key": "4", "value": "fields:keepCategory.4" }] }, { "disabled": false, "label": "inventory:fieldNames.name", "name": "name", "required": true, "requiredMessage": "inventory:formErrors.name", "type": "FieldText", "value": null, "additionalText": null, "allowClear": false, "maxLength": null }, { "disabled": false, "label": "inventory:fieldNames.note", "name": "note", "required": false, "requiredMessage": null, "type": "FieldMultiText", "value": null, "maxLength": null, "rows": null }, { "disabled": false, "label": "inventory:fieldNames.startYear", "name": "startYear", "required": true, "requiredMessage": "inventory:formErrors.startYear", "type": "FieldDateTime", "value": null, "dateTimeMode": 1, "maxDate": "2999", "minDate": "1753" }, { "disabled": false, "label": "inventory:fieldNames.endYear", "name": "endYear", "required": false, "requiredMessage": null, "type": "FieldDateTime", "value": null, "dateTimeMode": 1, "maxDate": "2999", "minDate": "1753" }],
-                "Mode": 2,
-                "Tabs": [{ "ClassName": null, "CustomType": null, "Disabled": false, "ForceRender": null, "Rows": [{ "Cells": [{ "Type": 0, "Fields": ["parentName"], "Width": 24 }] }, { "Cells": [{ "Type": 0, "Fields": ["ind"], "Width": 8 }, { "Type": 0, "Fields": ["volumeNum"], "Width": 4 }, { "Type": 0, "Fields": ["keepCategory"], "Width": 12 }] }, { "Cells": [{ "Type": 0, "Fields": ["name"], "Width": 24 }] }, { "Cells": [{ "Type": 0, "Fields": ["note"], "Width": 24 }] }, { "Cells": [{ "Type": 0, "Fields": ["startYear"], "Width": 6 }, { "Type": 0, "Fields": ["endYear"], "Width": 6 }] }], "Title": "tabs:inventory" }, { "ClassName": null, "CustomType": "InventoryFiles", "Disabled": false, "ForceRender": null, "Rows": null, "Title": "tabs:files" }]
+                "Fields": Helper.getFields(FormMode.display),
+                "Mode": FormMode.display,
+                "Tabs": Helper.getTabs()
             };
             const editContext = {
-                "Fields": [{ "disabled": true, "label": "inventory:fieldNames.parentName", "name": "parentName", "required": false, "requiredMessage": null, "type": "FieldText", "value": null, "additionalText": null, "allowClear": false, "maxLength": null }, { "disabled": false, "label": "inventory:fieldNames.ind", "name": "ind", "required": true, "requiredMessage": "inventory:formErrors.ind", "type": "FieldText", "value": null, "additionalText": null, "allowClear": true, "maxLength": 24 }, { "disabled": false, "label": "inventory:fieldNames.volumeNum", "name": "volumeNum", "required": false, "requiredMessage": null, "type": "FieldInteger", "value": null, "max": 32767, "min": 0, "showCounter": false }, { "disabled": false, "label": "inventory:fieldNames.keepCategory", "name": "keepCategory", "required": true, "requiredMessage": "inventory:formErrors.keepCategory", "type": "FieldSelect", "value": null, "values": [{ "key": "1", "value": "fields:keepCategory.1" }, { "key": "2", "value": "fields:keepCategory.2" }, { "key": "3", "value": "fields:keepCategory.3" }, { "key": "4", "value": "fields:keepCategory.4" }] }, { "disabled": false, "label": "inventory:fieldNames.name", "name": "name", "required": true, "requiredMessage": "inventory:formErrors.name", "type": "FieldText", "value": null, "additionalText": null, "allowClear": false, "maxLength": null }, { "disabled": false, "label": "inventory:fieldNames.note", "name": "note", "required": false, "requiredMessage": null, "type": "FieldMultiText", "value": null, "maxLength": null, "rows": null }, { "disabled": false, "label": "inventory:fieldNames.startYear", "name": "startYear", "required": true, "requiredMessage": "inventory:formErrors.startYear", "type": "FieldDateTime", "value": null, "dateTimeMode": 1, "maxDate": "2999", "minDate": "1753" }, { "disabled": false, "label": "inventory:fieldNames.endYear", "name": "endYear", "required": false, "requiredMessage": null, "type": "FieldDateTime", "value": null, "dateTimeMode": 1, "maxDate": "2999", "minDate": "1753" }],
-                "Mode": 1,
-                "Tabs": [
-                    {
-                        "ClassName": null, "CustomType": null, "Disabled": false, "ForceRender": null,
-                        "Rows": [{ "Cells": [{ "Type": 0, "Fields": ["parentName"], "Width": 24 }] }, { "Cells": [{ "Type": 0, "Fields": ["ind"], "Width": 8 }, { "Type": 0, "Fields": ["volumeNum"], "Width": 4 }, { "Type": 0, "Fields": ["keepCategory"], "Width": 12 }] }, { "Cells": [{ "Type": 0, "Fields": ["name"], "Width": 24 }] }, { "Cells": [{ "Type": 0, "Fields": ["note"], "Width": 24 }] }, { "Cells": [{ "Type": 0, "Fields": ["startYear"], "Width": 6 }, { "Type": 0, "Fields": ["endYear"], "Width": 6 }] }], "Title": "tabs:inventory"
-                    },
-                    {
-                        "ClassName": null, "CustomType": "InventoryFiles", "Disabled": true, "ForceRender": null, "Rows": null, "Title": "tabs:files"
-                    }]
+                "Fields": Helper.getFields(FormMode.edit),
+                "Mode": FormMode.edit,
+                "Tabs": Helper.getTabs()
             };
             const newContext = {
-                "Fields": [
-                    { type: "FieldCheckbox", name: "E_DOCUMENT", label: " ", description: E_DOCUMENT_LABEL },
-                    { "disabled": true, "label": "inventory:fieldNames.parentName", "name": "parentName", "required": false, "requiredMessage": null, "type": "FieldText", "value": null, "additionalText": null, "allowClear": false, "maxLength": null },
-                    { "disabled": false, "label": "inventory:fieldNames.ind", "name": "ind", "required": true, "requiredMessage": "inventory:formErrors.ind", "type": "FieldText", "value": null, "additionalText": null, "allowClear": true, "maxLength": 24 }, { "disabled": false, "label": "inventory:fieldNames.volumeNum", "name": "volumeNum", "required": false, "requiredMessage": null, "type": "FieldInteger", "value": null, "max": 32767, "min": 0, "showCounter": false }, { "disabled": false, "label": "inventory:fieldNames.keepCategory", "name": "keepCategory", "required": true, "requiredMessage": "inventory:formErrors.keepCategory", "type": "FieldSelect", "value": null, "values": [{ "key": "1", "value": "fields:keepCategory.1" }, { "key": "2", "value": "fields:keepCategory.2" }, { "key": "3", "value": "fields:keepCategory.3" }, { "key": "4", "value": "fields:keepCategory.4" }] }, { "disabled": false, "label": "inventory:fieldNames.name", "name": "name", "required": true, "requiredMessage": "inventory:formErrors.name", "type": "FieldText", "value": null, "additionalText": null, "allowClear": false, "maxLength": null }, { "disabled": false, "label": "inventory:fieldNames.note", "name": "note", "required": false, "requiredMessage": null, "type": "FieldMultiText", "value": null, "maxLength": null, "rows": null }, { "disabled": false, "label": "inventory:fieldNames.startYear", "name": "startYear", "required": true, "requiredMessage": "inventory:formErrors.startYear", "type": "FieldDateTime", "value": null, "dateTimeMode": 1, "maxDate": "2999", "minDate": "1753" }, { "disabled": false, "label": "inventory:fieldNames.endYear", "name": "endYear", "required": false, "requiredMessage": null, "type": "FieldDateTime", "value": null, "dateTimeMode": 1, "maxDate": "2999", "minDate": "1753" }],
-                "Mode": 0,
-                "Tabs": [{
-                    "ClassName": null, "CustomType": null, "Disabled": false, "ForceRender": null,
-                    "Rows": [
-                        { "Cells": [{ "Type": 0, "Fields": ["parentName"], "Width": 24 }] },
-                        { "Cells": [{ "Type": 0, "Fields": ["ind"], "Width": 8 }, { "Type": 0, "Fields": ["volumeNum"], "Width": 4 }, { "Type": 0, "Fields": ["keepCategory"], "Width": 12 }] },
-                        { "Cells": [{ "Type": 0, "Fields": ["name"], "Width": 24 }] },
-                        { "Cells": [{ "Type": 0, "Fields": ["note"], "Width": 24 }] },
-                        { "Cells": [{ "Type": 0, "Fields": ["startYear"], "Width": 6 }, { "Type": 0, "Fields": ["endYear"], "Width": 6 }] },
-                        { "Cells": [{ "Type": 0, "Fields": ["E_DOCUMENT"], "Width": 24 }] },
-                    ], "Title": "tabs:inventory"
-                }, { "ClassName": null, "CustomType": "InventoryFiles", "Disabled": true, "ForceRender": null, "Rows": null, "Title": "tabs:files" }]
+                "Fields": Helper.getFields(FormMode.new),
+                "Mode": FormMode.new,
+                "Tabs": Helper.getTabs()
             };
-            await sleep(1000);
+            await Helper.sleepAsync(100);
             switch (mode) {
                 case FormMode.edit:
                     return editContext;
@@ -57,11 +46,11 @@ const AjaxClientFormPage: FunctionComponent = () => {
             }
         },
         async getInitialValuesAsync() {
-            await sleep(1000);
-            return {};
+            await Helper.sleepAsync(100);
+            return Helper.getInitialValues(mode, id ?? 0);
         },
         getTitle: function () {
-            return "Форма";
+            return `Форма ${id ?? ""}`;
         }
     }
 
@@ -69,13 +58,9 @@ const AjaxClientFormPage: FunctionComponent = () => {
 
     return (
         <React.Fragment>
-            <Button onClick={() => {
-                formApi?.current?.showLoading();
-                setTimeout(() => { formApi?.current?.hideLoading(); }, 1500);
-            }}>Грузить</Button>
             <AjaxClientForm.Form
                 ref={formApi}
-                mode={FormMode.new}
+                mode={mode}
                 dataService={dataService}
                 getResourceText={name => name}
                 enableLeftIcon={true}
@@ -89,13 +74,13 @@ const AjaxClientFormPage: FunctionComponent = () => {
                             formApi?.current?.hideLeftIcon();
                     }
                 }}
+                onCancelClick={() => {
+                    formApi?.current?.showLoading();
+                    setTimeout(() => { formApi?.current?.hideLoading(); }, 1500);
+                }}
             />
         </React.Fragment>
     );
-
-    async function sleep(msec: number) {
-        return new Promise(resolve => setTimeout(resolve, msec));
-    }
 }
 
 export default AjaxClientFormPage;
